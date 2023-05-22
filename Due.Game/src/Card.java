11a9d15f2d5bec1e7968f416a8fc8fce4934ca75
DavidGrb <david.gruebling@gmx.at>
2023-05-22 12:25:39 +0200

    deck class

--- Due.Game/src/Card.java
+++ Due.Game/src/Card.java
@@ -1,4 +1,25 @@
 public class Card {
     private String color;
     private int number;
+
+    public Card(String color, int number) {
+        this.color = color;
+        this.number = number;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
 }
---
+++ Due.Game/src/Deck.java
@@ -0,0 +1,11 @@
+public class Deck {
+
+    public Deck(int cards) {
+        //füllt ein deck mit zufällig generierten karten
+        for (int i = 0; i < cards; i++) {
+            String[] colors = {"Red, Blue, Green, Yellow"};
+            int[] numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        }
+    }
+}
